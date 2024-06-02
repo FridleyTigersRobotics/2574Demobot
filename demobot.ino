dbc0e08eefa614adf6ab79632251ad424221c18a
Terrrm <9992188+Terrrm@users.noreply.github.com>
2024-06-02 01:01:26 -0500

    Add defines for pin numbers

--- demobot.ino
+++ demobot.ino
@@ -3,33 +3,47 @@ Servo servoch1;
 Servo servoch2;
 bool readerror=false;
 
+#define PIN_CTRL_DRIVE_X      ( 2 )
+#define PIN_CTRL_DRIVE_Y      ( 3 )
+#define PIN_CTRL_HORN         ( 4 )
+#define PIN_CTRL_LIGHT        ( 5 )
+#define PIN_CTRL_ROBOT_ENABLE ( 6 )
+#define PIN_UNUSED            ( 7 )
+#define PIN_SERVO1            ( 8 )
+#define PIN_SERVO_RESERVED_0  ( 9 )
+#define PIN_SERVO_RESERVED_1  ( 10 )
+#define PIN_SERVO2            ( 11 )
+#define PIN_HORN              ( 12 )
+#define PIN_LIGHTS            ( 13 )
+
+
 void setup() {
-pinMode(2, INPUT);
-pinMode(3, INPUT);
-pinMode(4, INPUT);
-pinMode(5, INPUT);
-pinMode(6, INPUT);
-pinMode(7, INPUT);//not used
-servoch1.attach(8);
+pinMode(PIN_CTRL_DRIVE_X, INPUT);
+pinMode(PIN_CTRL_DRIVE_Y, INPUT);
+pinMode(PIN_CTRL_HORN, INPUT);
+pinMode(PIN_CTRL_LIGHT, INPUT);
+pinMode(PIN_CTRL_ROBOT_ENABLE, INPUT);
+pinMode(PIN_UNUSED, INPUT);//not used
+servoch1.attach(PIN_SERVO1);
 //pins 9 and 10 used for timing in servo library
-servoch2.attach(11);
-pinMode(12, OUTPUT);
-pinMode(13, OUTPUT);
-digitalWrite(12,LOW);
-digitalWrite(13,LOW);
+servoch2.attach(PIN_SERVO2);
+pinMode(PIN_HORN, OUTPUT);
+pinMode(PIN_LIGHTS, OUTPUT);
+digitalWrite(PIN_HORN,LOW);
+digitalWrite(PIN_LIGHTS,LOW);
 Serial.begin(9600);
 }
 
 void loop() {
   int rightmotor=0;
   int leftmotor=0;
-  int ch1 = pulseIn(2, HIGH); //read the pulse width of each channel
-  int ch2 = pulseIn(3, HIGH);
-  int ch3 = pulseIn(4, HIGH);
-  int ch4 = pulseIn(5, HIGH);
-  int ch5 = pulseIn(6, HIGH);
+  int ch1 = pulseIn(PIN_CTRL_DRIVE_X, HIGH); //read the pulse width of each channel
+  int ch2 = pulseIn(PIN_CTRL_DRIVE_Y, HIGH);
+  int ch3 = pulseIn(PIN_CTRL_HORN, HIGH);
+  int ch4 = pulseIn(PIN_CTRL_LIGHT, HIGH);
+  int ch5 = pulseIn(PIN_CTRL_ROBOT_ENABLE, HIGH);
   if(ch1==0||ch2==0||ch3==0||ch4==0||ch5==0) readerror=true;
-  //int ch6 = pulseIn(7, HIGH);
+  //int ch6 = pulseIn(PIN_UNUSED, HIGH);
   ch1=map(ch1, 1100,1875,-100,100);//tested max limits of each channel
   ch2=map(ch2, 1198,1870,-100,100);
   ch3=map(ch3, 1139,1820,-100,100);
@@ -47,22 +61,22 @@ void loop() {
     leftmotor=ch2+ch1;
     servoch1.writeMicroseconds(victormap(leftmotor,false));
     servoch2.writeMicroseconds(victormap(rightmotor,false));
-    if (ch3>50) digitalWrite(12,HIGH);//blow horn
-    else digitalWrite(12,LOW);
-    if(ch4>50||ch4<-50) digitalWrite(13,HIGH);//turn on lights
-    else digitalWrite(13,LOW);
+    if (ch3>50) digitalWrite(PIN_HORN,HIGH);//blow horn
+    else digitalWrite(PIN_HORN,LOW);
+    if(ch4>50||ch4<-50) digitalWrite(PIN_LIGHTS,HIGH);//turn on lights
+    else digitalWrite(PIN_LIGHTS,LOW);
   }
   else{
     servoch1.writeMicroseconds(1500);
     servoch2.writeMicroseconds(1500);
-    digitalWrite(12,LOW);
-    digitalWrite(13,LOW);
+    digitalWrite(PIN_HORN,LOW);
+    digitalWrite(PIN_LIGHTS,LOW);
     if(readerror==true) Serial.println("Channel Read Error");
-    int ch1 = pulseIn(2, HIGH); //read the pulse width of each channel
-    int ch2 = pulseIn(3, HIGH);
-    int ch3 = pulseIn(4, HIGH);
-    int ch4 = pulseIn(5, HIGH);
-    int ch5 = pulseIn(6, HIGH);
+    int ch1 = pulseIn(PIN_CTRL_DRIVE_X, HIGH); //read the pulse width of each channel
+    int ch2 = pulseIn(PIN_CTRL_DRIVE_Y, HIGH);
+    int ch3 = pulseIn(PIN_CTRL_HORN, HIGH);
+    int ch4 = pulseIn(PIN_CTRL_LIGHT, HIGH);
+    int ch5 = pulseIn(PIN_CTRL_ROBOT_ENABLE, HIGH);
     readerror=false;
     if(ch1==0||ch2==0||ch3==0||ch4==0||ch5==0) readerror=true;
   }
